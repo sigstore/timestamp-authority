8229237f4cf88a09a20550fe049675db7fdd2d36
Meredith Lancaster <malancas@github.com>
2022-10-07 10:22:44 -0600

    remove unused middleware
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- pkg/generated/restapi/configure_timestamp_server.go
+++ pkg/generated/restapi/configure_timestamp_server.go
@@ -71,7 +71,6 @@ func configureAPI(api *operations.TimestampServerAPI) http.Handler {
 
 	api.ServerShutdown = func() {}
 
-	api.AddMiddlewareFor("GET", "/api/v1/timestamp", middleware.NoCache)
 	api.AddMiddlewareFor("GET", "/api/v1/log/timestamp/certchain", cacheForDay)
 
 	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
