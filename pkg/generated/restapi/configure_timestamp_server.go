a938190e18fe0e10df5a4ec0c9c2eef31ff0193f
Meredith Lancaster <malancas@github.com>
2022-10-11 18:21:55 -0600

    remove unimplemented metric
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- pkg/generated/restapi/configure_timestamp_server.go
+++ pkg/generated/restapi/configure_timestamp_server.go
@@ -148,13 +148,6 @@ func wrapMetrics(handler http.Handler) http.Handler {
 				"path":   r.URL.Path,
 				"method": r.Method,
 			}).Observe(float64(time.Since(start)))
-
-			pkgapi.MetricRequestCount.With(
-				map[string]string{
-					"path":   r.URL.Path,
-					"method": r.Method,
-					"code":   strconv.Itoa(ww.Status()),
-				}).Inc()
 		}()
 
 		handler.ServeHTTP(ww, r)
