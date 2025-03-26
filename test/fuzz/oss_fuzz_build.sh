6b527e7140a91160476ab279ff6d4bc10a487e82
Bob Callaway <bobcallaway@users.noreply.github.com>
2025-03-26 12:44:54 -0400

    add initial fuzzing targets (#1013)
    * initial fuzz targets
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>
    
    * use compile_go_fuzzer
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>
    
    * quiet zip command
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>
    
    * switch back to native_go
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>
    
    * fix lint
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>
    
    ---------
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>

---
+++ test/fuzz/oss_fuzz_build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+go get github.com/AdamKorcz/go-118-fuzz-build/testing
+
+compile_native_go_fuzzer github.com/sigstore/timestamp-authority/pkg/api FuzzParseJSONRequest FuzzParseJSONRequest
+compile_native_go_fuzzer github.com/sigstore/timestamp-authority/pkg/api FuzzParseDERRequest FuzzParseDERRequest
+
+zip -qj $OUT/FuzzParseJSONRequest_seed_corpus.zip $SRC/go-fuzz-corpus/json/corpus/*
+cp $SRC/afl-fuzz/dictionaries/json.dict $OUT/FuzzParseJSONRequest.dict
